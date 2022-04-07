e73ef492183be75c9a28a6a6564fb38eeeb31b58
Jamond Turner <jamondturner@yahoo.com>
2022-04-07 17:04:22 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -21,17 +21,20 @@ function decrement(a) {
     return a - 1
 }
 
+
+//  Finish Below Later On-------- parseFloat() only accepts a single argument and preserves decimals
+//  parseInt() only returns integers lops of everything after the decimal point
+
+
+// makeInt string take in a string parse it into a base 10 integer and return it.
 function makeInt(string) {
-    return string
+    return parseInt(string, 10)
 }
 
-function preserveDecimal(a){
-    return (parseInt(a,10))
-
-}
+// Take in a string parse it into a float and return it
 
 function preserveDecimal(string){
-    return parseInt(2, 10)
+    return parseFloat(string)
 }
 
 
